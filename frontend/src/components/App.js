ff52258f035bf6d93e47228a63864db74f391a11
James Vuong <jvuong@mckinley.com>
2018-03-10 14:07:16 -0500

    Initial API interface, render links from categories

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,36 +1,49 @@
 import React, { Component } from 'react';
 import Post from './Post'
+import * as ReadableAPI from '../utils/ReadableAPI'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div id="layout">
+    state = {
+        categories: []
+    }
 
-            <div id="menu">
-                <div className="pure-menu">
-                    <a className="pure-menu-heading" href="#">Readable</a>
-                    <ul className="pure-menu-list">
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Category 1</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Category 2</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Category 3</a></li>
-                    </ul>
+    componentDidMount() {
+        ReadableAPI.getAllCategories().then((categories) =>
+            this.setState({categories})
+        )
+    }
+
+    render() {
+        const { categories } = this.state
+
+        return (
+            <div className="App">
+                <div id="layout">
+
+                    <div id="menu">
+                        <div className="pure-menu">
+                            <a className="pure-menu-heading" href="#">Readable</a>
+                            <ul className="pure-menu-list">
+                                { categories.map((category) => (
+                                    <li className="pure-menu-item"><a href={category.path} className="pure-menu-link">{ category.name }</a></li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div id="main">
+                        <div className="header">
+                            <h1>Readable</h1>
+                        </div>
+
+                        <Post />
+                        <Post />
+                        <Post />
+                    </div>
                 </div>
             </div>
-
-            <div id="main">
-                <div className="header">
-                    <h1>Readable</h1>
-                </div>
-
-                <Post />
-                <Post />
-                <Post />
-            </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
