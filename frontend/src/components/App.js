9410895d249913fe3149584400f1a030eb15b604
James Vuong <jvuong@mckinley.com>
2018-03-15 22:57:53 -0400

    FEATURE: Post Sorting by Votes or Date

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import Post from './Post'
 import * as ReadableAPI from '../utils/ReadableAPI'
 
+const SortDirection = {
+    ASCENDING : 'asc',
+    DESCENDING : 'desc'
+}
+
 class App extends Component {
+
     state = {
         categories: [],
         posts: []
@@ -16,6 +22,30 @@ class App extends Component {
         ReadableAPI.getAllPosts().then((posts) => 
             this.setState({posts})
         )
+    }
+
+    togglePostVoteSort = (direction) => {
+        let posts = []
+        // Ascending = display lowest votes first ( smaller votes first )
+        // Descending = display most votes first ( bigger votes first )
+        if(direction === SortDirection.ASCENDING) {
+            posts = this.state.posts.slice(0).sort((a,b) => a.voteScore < b.voteScore)
+        } else {
+            posts = this.state.posts.slice(0).sort((a,b) => a.voteScore > b.voteScore)
+        }
+        this.setState({posts})
+    }
+
+    togglePostDateSort = (direction) => {
+        let posts = []
+        // Ascending = display older posts first ( smaller timestamps first )
+        // Descending = display new posts first ( bigger timestamps first )
+        if(direction === SortDirection.ASCENDING) {
+            posts = this.state.posts.slice(0).sort((a,b) => a.timestamp < b.timestamp)
+        } else {
+            posts = this.state.posts.slice(0).sort((a,b) => a.timestamp > b.timestamp)
+        }
+        this.setState({posts})
     }
 
     render() {
@@ -39,6 +69,14 @@ class App extends Component {
                     <div id="main">
                         <div className="header">
                             <h1>Readable Test</h1>
+                        </div>
+
+                        <div id="sort-bar">
+                            Order Posts By: 
+                            <button onClick={() => this.togglePostVoteSort(SortDirection.ASCENDING)}>Most Votes</button>
+                            <button onClick={() => this.togglePostVoteSort(SortDirection.DESCENDING)}>Least Votes</button> | 
+                            <button onClick={() => this.togglePostDateSort(SortDirection.ASCENDING)}>Most Recent</button>
+                            <button onClick={() => this.togglePostDateSort(SortDirection.DESCENDING)}>Oldest</button>
                         </div>
 
                         { posts.map((post) => (
