c356d7299bddad5b72f31880949216af8cb9f4bd
James Vuong <jvuong@mckinley.com>
2018-03-17 15:03:47 -0400

    Add Post Creation Component / Implement React Router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
 import Post from './Post'
 import * as ReadableAPI from '../utils/ReadableAPI'
+import CreateEditPost from './CreateEditPost'
 
 const SortDirection = {
     ASCENDING : 'asc',
@@ -59,39 +61,48 @@ class App extends Component {
                         <div className="pure-menu">
                             <a className="pure-menu-heading" href="#">Readable</a>
                             <ul className="pure-menu-list">
+                                <li className="pure-menu-item"><a href="/" className="pure-menu-link">Home</a></li>
                                 { categories.map((category) => (
                                     <li key={category.name} className="pure-menu-item"><a href={category.path} className="pure-menu-link">{ category.name }</a></li>
                                 ))}
+                                <li className="pure-menu-item"><a href="/create" className="pure-menu-link">Create a Post</a></li>
                             </ul>
                         </div>
                     </div>
 
                     <div id="main">
-                        <div className="header">
-                            <h1>Readable Test</h1>
-                        </div>
+                        <Route exact path="/" render={() =>
+                            <div>
+                                <div className="header">
+                                    <h1>Readable Test</h1>
+                                </div>
 
-                        <div id="sort-bar">
-                            Order Posts By: 
-                            <button onClick={() => this.togglePostVoteSort(SortDirection.ASCENDING)}>Most Votes</button>
-                            <button onClick={() => this.togglePostVoteSort(SortDirection.DESCENDING)}>Least Votes</button> | 
-                            <button onClick={() => this.togglePostDateSort(SortDirection.ASCENDING)}>Most Recent</button>
-                            <button onClick={() => this.togglePostDateSort(SortDirection.DESCENDING)}>Oldest</button>
-                        </div>
+                                <div id="sort-bar">
+                                    Order Posts By: 
+                                    <button onClick={() => this.togglePostVoteSort(SortDirection.ASCENDING)}>Most Votes</button>
+                                    <button onClick={() => this.togglePostVoteSort(SortDirection.DESCENDING)}>Least Votes</button> | 
+                                    <button onClick={() => this.togglePostDateSort(SortDirection.ASCENDING)}>Most Recent</button>
+                                    <button onClick={() => this.togglePostDateSort(SortDirection.DESCENDING)}>Oldest</button>
+                                </div>
 
-                        { posts.map((post) => (
-                            <Post 
-                                key={post.id}
-                                id={post.id}
-                                author={post.author}
-                                title={post.title}
-                                body={post.body}
-                                category={post.category}
-                                commentCount={post.commentCount}
-                                createdAt={post.timestamp}
-                                voteScore={post.voteScore}
-                            />
-                        ))}
+                                { posts.map((post) => (
+                                    <Post 
+                                        key={post.id}
+                                        id={post.id}
+                                        author={post.author}
+                                        title={post.title}
+                                        body={post.body}
+                                        category={post.category}
+                                        commentCount={post.commentCount}
+                                        createdAt={post.timestamp}
+                                        voteScore={post.voteScore}
+                                    />
+                                ))}
+                            </div>
+                        } />
+                        <Route exact path="/create" render={() =>
+                            <CreateEditPost categories={categories} />
+                        } />
                     </div>
                 </div>
             </div>
---
+++ frontend/src/components/CreateEditPost.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import * as ReadableAPI from '../utils/ReadableAPI'
+
+class CreateEditPost extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: '',
+            body: '',
+            author: '',
+            category: ''
+        }
+    }
+
+    handleInputChange = (e) => {
+        const target = e.target
+        const value = target.value 
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        
+        const uuidv4 = require('uuid/v4');
+
+        let post = {
+            id: uuidv4(),
+            timestamp: Date.now(),
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author,
+            category: this.state.category,
+        }
+
+        ReadableAPI.addPost(post)
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    title: '',
+                    body: '',
+                    author: '',
+                    category: ''
+                })
+            })
+    }
+
+    render() {
+        const {categories} = this.props
+        return (
+            <div>
+                <h2 className="content-subhead">Create a new Post</h2>
+                <form id="postForm" name="postForm" action='POST' onSubmit={this.handleFormSubmit}>
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" name="title" value={this.state.title}  onChange={this.handleInputChange} />
+                    <br/>
+                    <label htmlFor="author">Author:</label>
+                    <input type="text" id="author" name="author" value={this.state.author}  onChange={this.handleInputChange} />
+                    <br/>
+                    <label htmlFor="body">Body:</label>
+                    <textarea id="body" name="body" value={this.state.body}  onChange={this.handleInputChange} />
+                    <br/>
+                    <label htmlFor="category">Category:</label>
+                    <select id="category" name="category" value={this.state.category} onChange={this.handleInputChange} >
+                        { categories.map((category) => (
+                            <option value={category.name}>{category.name}</option>
+                        )) }
+                    </select>
+                    <br/>
+                    <input type="submit" id="submit" name="submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CreateEditPost
